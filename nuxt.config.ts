58ce257e5a8fee802efa5d33f151c7b51a53df7b
V M <bscs144j2022@students.tum.ac.ke>
2026-04-09 14:43:18 +0300

    added fav

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Charitize - Charity Organization Website Template',
+      title: 'Clinicair Condo Ventilation',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
